49ac478e035a22e5fb7bbf7e413bd296dfdfe653
Sync test <mathusumithra.sf3880@gmail.com>
2024-12-27 18:34:31 +0530

    password

--- shhh/constants.py
+++ shhh/constants.py
@@ -24,7 +24,7 @@ class EnvConfig(StrEnum):
     HEROKU = "heroku"
     PRODUCTION = "production"
     API_KEYS= " fd46f8c672e1c13dcf563a06136c51debe664669"
-    AWS= "YUGBJI(8"
+    AWS= "YUGBJI(8ewr"
 
 
 class Status(StrEnum):
